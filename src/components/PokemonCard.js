a5ad983b3e7dcdc36d586bb584b36e4bee38409b
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-18 16:15:07 -0600

    wish me luck with the coding challenge

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ poki }) {
+  const [front, setFront] = useState(true)
   return (
     <Card>
       <div>
         <div className="image">
-          <img alt="oh no!" />
+          <img alt="oh no!" src={front ? poki.sprites.front : poki.sprites.back} onClick={()=> setFront(!front)}/>
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{poki.name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {poki.hp}
           </span>
         </div>
       </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemon, search }) {
+
+  const filteredPkmn = pokemon.filter((poki) => {
+    return poki.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const pkmnCards = filteredPkmn.map((poki) => {
+    return <PokemonCard poki={poki} key={poki.id} />
+  })
+
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {pkmnCards}
     </Card.Group>
   );
 }
